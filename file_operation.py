59ae61eda69fd95e5368403e48efd754c493f180
TechNote <technote.git@gmail.com>
2021-09-20 17:27:52 +0900

    Basic implemented code

--- file_operation.py
+++ file_operation.py
@@ -1,9 +1,23 @@
-def file_read():
-    return 0
+from datetime import datetime
+import pickle
+import os
 
-def file_write():
-    return 0
+
+def file_read(target_file):
+    with open(target_file, 'rb') as f:
+        return pickle.load(f)
+
+
+def file_write(target_file):
+    current_time = datetime.now()
+    with open(target_file, 'wb') as f:
+        pickle.dump(current_time, f)
+
 
 if __name__ == '__main__':
-    file_read()
-    file_write()
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    target_file = current_path + '/test_file'
+
+    file_write(target_file)
+    now = file_read(target_file)
+    print(str(now))
