005e715e648b6cdcad67d2ff1284c8d57be57715
TechNote <technote.git@gmail.com>
2021-09-20 21:14:16 +0900

    Write unittest with mock_open

--- test/test_file_operation.py
+++ test/test_file_operation.py
@@ -1,15 +1,27 @@
 import unittest
 import file_operation
+import pickle
+from datetime import datetime
+from unittest.mock import Mock
+from unittest.mock import patch, mock_open
 
 
 class TestFileOperation(unittest.TestCase):
     def test_file_read(self):
-        ret = file_operation.file_read()
-        self.assertEqual(0, ret)
+        current_time = datetime.now()
+        read_data = pickle.dumps(current_time)
+        mock = mock_open(read_data=read_data)
+        with patch('builtins.open', mock):
+            ret = file_operation.file_read('test_file')
+
+        self.assertEqual(current_time, ret)
 
     def test_file_write(self):
-        ret = file_operation.file_write()
-        self.assertEqual(0, ret)
+        mock = mock_open()
+        with patch('builtins.open', mock):
+            file_operation.file_write('test_file')
+
+        mock.assert_called_once_with('test_file', 'wb')
 
 
 if __name__ == '__main__':
